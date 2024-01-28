f225a8f0246b36704b717d0a9c586f232e5ce9f8
briseldh <briseld.hoxha14@gmail.com>
2024-01-28 23:43:16 +0100

    A small change in GetDataConroller

--- app/Http/Controllers/GetDataController.php
+++ app/Http/Controllers/GetDataController.php
@@ -27,13 +27,13 @@ class GetDataController extends Controller
         $user = $request->user();
         $posts = $user->posts;
         $comments = $user->comments;
-        $uploads = DB::select('select * from project_backend.post_images where post_id = :postId', ['postId' => 37]);
+        $uploads = DB::select('select * from project_backend.post_images where user_id = :userId', ['userId' => $request->user()->id]);
 
         // $user = User::with('posts', 'comments')->get();
         // $posts = Post::with('file')->get();
 
 
-        return response()->json(['message' => 'GetUserData successful', 'userData' => [$user, $uploads]], 200);
+        return response()->json(['message' => 'GetUserData successful', 'userData' => $user, 'userUploads' => $uploads], 200);
     }
 
     public function getUserById(string $id)
